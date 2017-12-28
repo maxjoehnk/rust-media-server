6d550c6633d8a866147386ebaf891712264160e3
Max Jöhnk <maxjoehnk@gmail.com>
2017-12-28 20:12:54 +0100

    export arc library

--- src/library/mod.rs
+++ src/library/mod.rs
@@ -4,8 +4,12 @@ mod library;
 mod playlist;
 mod track;
 
+use std::sync::{Mutex, Arc};
+
 pub use self::album::Album;
 pub use self::artist::Artist;
 pub use self::library::Library;
 pub use self::playlist::Playlist;
 pub use self::track::Track;
+
+pub type GlobalLibrary = Arc<Mutex<Library>>;
